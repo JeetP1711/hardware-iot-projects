9b367a4068ef71b384fb0ed22dbf3a008cb96dca
Jeet Patel <157380070+JeetP1711@users.noreply.github.com>
2026-07-01 22:14:04 +0530

    Update led-blink.py

--- led-blink.py
+++ led-blink.py
@@ -1,1 +1,12 @@
+import RPi.GPIO as GPIO
+from time import sleep
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(4,GPIO.OUT)
+
+while True:
+  GPIO.output(4,GPIO.HIGH)
+  sleep(1)
+  GPIO.output(4,GPIO.LOW)
+  sleep(1)
